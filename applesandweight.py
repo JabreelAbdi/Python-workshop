4a028a9f7aafdac2603a520b74008e0f05acca80
Jabreel Abdi <jabreelabdi@Jabreels-MacBook-Pro.local>
2021-11-10 21:09:30 +0000

    second draft of code

--- applesandweight.py
+++ applesandweight.py
@@ -1,11 +1,15 @@
 one_apple_weight_kg = 0.2
 one_apple_pounds_kg = 0.440925
-amount_apples = input("how many apples do you have? ")
-print(amount_apples)
+amount_apples = int(input("How many apples do you have? "))
+weight_selection = input("Is that in Kilos (kg) or Pounds (lbs)? ").upper()
+# print(amount_apples)
+# print(weight_selection)
 
-kg_amount = int(amount_apples) * one_apple_weight_kg
-print(kg_amount)
+if weight_selection == "KG":
+    kg_amount = amount_apples * one_apple_weight_kg
+    print("Your apples in KG is " + str(kg_amount))
 
-lbs_amount = int(amount_apples) * one_apple_pounds_kg
-print(lbs_amount)
+if weight_selection == "LBS":
+    lbs_amount = amount_apples * one_apple_pounds_kg
+    print("Your apples in LBS is " + str(lbs_amount))
 
